f0ffe1a7fc0100e90f917ab358477f6ff5f5bb0e
@123umair <khanuumair91@gmail.com>
2026-02-18 11:06:43 +0500

    Initial commit

---
+++ MyApp/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import { useFonts } from 'expo-font'
+import { ActivityIndicator } from "react-native";
+
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'appFont':require('./../assets/fonts/Montserrat-Regular.ttf'),
+    'appFontBold':require('./../assets/fonts/Montserrat-Bold.ttf'),
+
+  })
+  if (!fontsLoaded){
+    return <ActivityIndicator/>
+  }
+  return <Stack />;
+}
